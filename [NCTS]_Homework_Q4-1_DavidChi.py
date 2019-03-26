4e943ca40b930724815f131ff7f63ebbe3a8dbd0
ChiDavid <48887627+ChiDavid@users.noreply.github.com>
2019-03-27 00:06:57 +0800

    Add files via upload

---
+++ [NCTS]_Homework_Q4-1_DavidChi.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd
+
+import matplotlib as mpl
+mpl.rc('font', family='Noto Sans CJK TC')
+[f.name for f in mpl.font_manager.fontManager.ttflist]
+
