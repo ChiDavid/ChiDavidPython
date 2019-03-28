8ed2efe6b1495c35fb99636a66c99ed16519668c
ChiDavid <48887627+ChiDavid@users.noreply.github.com>
2019-03-29 06:33:34 +0800

    Update [NCTS]_Homework_Q4-1_DavidChi.py

--- [NCTS]_Homework_Q4-1_DavidChi.py
+++ [NCTS]_Homework_Q4-1_DavidChi.py
@@ -1,9 +1,19 @@
-get_ipython().run_line_magic('matplotlib', 'inline')
-import numpy as np 
-import matplotlib.pyplot as plt 
-import pandas as pd
+%matplotlib inline
 
+import numpy as np
+import matplotlib.pyplot as plt
 import matplotlib as mpl
-mpl.rc('font', family='Noto Sans CJK TC')
-[f.name for f in mpl.font_manager.fontManager.ttflist]
+import pandas as pd
+import locale
+import time
+from ipywidgets import interact
 
+path = ('http://datacenter.taichung.gov.tw/swagger/OpenData/d9cad116-1255-4e97-94fc-d8dc58cd9ff9')
+df = pd.read_csv(path)
+
+mpl.rc('font', family='Noto Sans CJK TC')
+
+df2 = df[(df.肇事逃逸.str.contains('否')) | (df.酒駕案件.str.contains('是'))]
+df3 = df2.sort_values(by = ['事故類別'], ascending=False)
+df3.hist(bins=30)
+df3.mean()
